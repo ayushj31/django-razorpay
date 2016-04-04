8139ab6c1bee9bf68bffede59539505dfa8dee53
Ayush Jain <ayush.jain@consultadd.in>
2016-04-04 16:37:41 +0530

    migration changes

--- payment_gateway/razorpay_gateway/models.py
+++ payment_gateway/razorpay_gateway/models.py
@@ -1,3 +1,10 @@
+# Core Django imports
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
+
+
+class PaymentDetails(models.Model):
+    razorpay_id = models.CharField(max_length=255)
+    payment = JSONField(null=True, blank=True)
--- payment_gateway/razorpay_gateway/views.py
+++ payment_gateway/razorpay_gateway/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from payment_gateway.razorpay_gateway.models import PaymentDetails
+
 # Create your views here.
 
 import razorpay
@@ -16,6 +18,8 @@ def checkout(request):
 def capture(request):
     razorpay_payment_id = request.POST.getlist('razorpay_payment_id')
     payment = razor.payment.capture(razorpay_payment_id[0], "5000")
+    payment_datail = PaymentDetails(razorpay_id=razorpay_payment_id[0], payment=payment)
+    payment_datail.save()
     context = {"payment": payment,
                "razorpay_payment_id": razorpay_payment_id}
     return render(request, "razorpay_gateway/capture.html", context)
